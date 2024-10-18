c71a811f06484db23fcb738070d0ce0428940284
Soton Ahmed <soton371@gmail.com>
2024-10-18 22:11:06 +0600

    end

--- app/main.py
+++ app/main.py
@@ -1,13 +1,20 @@
 from fastapi import FastAPI
-# from . import models
-# from .database import engine
 from .routers import post, user, auth, vote
+from fastapi.middleware.cors import CORSMiddleware
 
 
 # models.Base.metadata.create_all(bind=engine) comment for alembic
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(post.router)
@@ -20,4 +27,4 @@ app.include_router(vote.router)
 async def root():
     return {"message": "Hello World"}
 
-#11:05
+#14:14
